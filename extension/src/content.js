7019d5ef6e8608b5b44510c5d2597aac59b5666b
8TBHomework <tduebel88@tutanota.com>
2021-11-02 22:19:45 +0100

    refactor: switch WebExt to new API

--- extension/src/content.js
+++ extension/src/content.js
@@ -1,50 +1,22 @@
-import sha256 from "crypto-js/sha256";
+function base64url(plaintext) {
+    return btoa(plaintext).replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+}
 
 function handleNewIMG(imgElem) {
     const imgUrl = imgElem.src;
     if (imgUrl) {
-        const imgId = sha256(imgUrl).toString();
-        browser.runtime.sendMessage({type: "enqueue", id: imgId, url: imgUrl});
-        imgElem.setAttribute("betamode", imgId);
+        imgElem.src = `http://localhost:8000/censored/${base64url(imgUrl)}`
+        imgElem.setAttribute("betamode", "1");
+        imgElem.removeAttribute("srcset");
     } // TODO: handle images with no src
 }
 
-browser.runtime.onMessage.addListener(msg => {
-        if (msg.type === "done") {
-            const elem = document.querySelector(`[betamode="${msg.id}"]`);
-            if (elem == null) { // element disappeared
-                return;
-            }
-            elem.addEventListener("load", () => {
-                elem.setAttribute("betamode-done", "1");
-            });
-            elem.src = msg.src;
-            elem.removeAttribute("srcset");
-        }
-    }
-);
 
 function queueImages() {
     for (const e of document.querySelectorAll("img:not([betamode])")) {
         handleNewIMG(e);
     }
 }
-
-const config = {attributes: true, childList: true, subtree: true};
-
-const observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-        if (mutation.type === "childList") {
-            mutation.removedNodes.forEach(node => {
-                if (node.hasAttribute("betamode") && !node.hasAttribute("betamode-done")) {
-                    chrome.runtime.sendMessage({type: "dequeue", id: node.getAttribute("betamode")});
-                }
-            });
-        }
-    });
-});
-
-observer.observe(document.body, config);
 
 setInterval(queueImages, 500);
 queueImages();
