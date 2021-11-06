cfabecb63360e35a648babbf11b33d0f5d1fb28e
8TBHomework <tduebel88@tutanota.com>
2021-11-06 01:31:58 +0100

    feat: use MutationObserver

--- extension/src/content.js
+++ extension/src/content.js
@@ -1,4 +1,4 @@
-const API_ENDPOINT = "http://localhost:8000/censored"
+const API_ENDPOINT = "http://localhost:8000/censored";
 
 function base64url(plaintext) {
     return btoa(plaintext).replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
@@ -7,18 +7,44 @@ function base64url(plaintext) {
 function handleNewIMG(imgElem) {
     const imgUrl = imgElem.src;
     if (imgUrl) {
-        imgElem.src = `${API_ENDPOINT}/${base64url(imgUrl)}`
+        imgElem.src = `${API_ENDPOINT}/${base64url(imgUrl)}`;
         imgElem.setAttribute("betamode", "1");
         imgElem.removeAttribute("srcset");
     } // TODO: handle images with no src
 }
 
 
-function queueImages() {
+function queueExistingImages() {
     for (const e of document.querySelectorAll("img:not([betamode])")) {
         handleNewIMG(e);
     }
 }
 
-setInterval(queueImages, 500);
-queueImages();
+function findImagesRecursively(element) {
+    let images = [];
+    if (element.tagName === "IMG" && !element.hasAttribute("betamode"))
+        images.push(element);
+
+    for (const child of element.childNodes) {
+        images = images.concat(findImagesRecursively(child));
+    }
+    return images;
+}
+
+const config = {childList: true, subtree: true};
+
+const callback = function (mutationsList, observer) {
+    for (const mutation of mutationsList) {
+        if (mutation.type === "childList") {
+            for (const e of mutation.addedNodes) {
+                const images = findImagesRecursively(e);
+                images.forEach(handleNewIMG);
+            }
+        }
+    }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(document, config);
+
+queueExistingImages();
