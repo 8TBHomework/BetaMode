f8256e055eac8efd2ef1215eeaea6569174c8561
8TBHomework <tduebel88@tutanota.com>
2021-11-06 02:07:46 +0100

    faet: implement background-image support

--- extension/src/content.js
+++ extension/src/content.js
@@ -1,4 +1,6 @@
 const API_ENDPOINT = "http://localhost:8000/censored";
+const BG_IMG_URL_PATTERN = /url\("(.*)"\)/;
+
 
 function base64url(plaintext) {
     return btoa(plaintext).replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
@@ -13,11 +15,23 @@ function handleNewIMG(imgElem) {
     } // TODO: handle images with no src
 }
 
+function handleNewBGIMG(imgElem) {
+    console.log(imgElem);
+    const bgIMG = getComputedStyle(imgElem).backgroundImage;
+    const match = bgIMG.match(BG_IMG_URL_PATTERN);
+    const imgUrl = match.length > 1 ? match[1] : null;
+    if (imgUrl) {
+        imgElem.style.backgroundImage = `url("${API_ENDPOINT}/${base64url(imgUrl)}")`;
+        imgElem.setAttribute("betamode", "1");
+    }
+}
 
 function queueExistingImages() {
     for (const e of document.querySelectorAll("img:not([betamode])")) {
         handleNewIMG(e);
     }
+    const images = findBackgroundImagesRecursively(document.body);
+    images.forEach(handleNewBGIMG);
 }
 
 function findImagesRecursively(element) {
@@ -27,6 +41,19 @@ function findImagesRecursively(element) {
 
     for (const child of element.childNodes) {
         images = images.concat(findImagesRecursively(child));
+    }
+    return images;
+}
+
+function findBackgroundImagesRecursively(element) {
+    let images = [];
+    if (element != null && element.nodeType === 1) {
+        if (getComputedStyle(element).backgroundImage !== "none" && !element.hasAttribute("betamode"))
+            images.push(element);
+
+        for (const child of element.childNodes) {
+            images = images.concat(findBackgroundImagesRecursively(child));
+        }
     }
     return images;
 }
